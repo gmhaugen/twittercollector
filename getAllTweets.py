77b5e40c7ca08b03e4a7df3dcc284fcb17c476d3
Geir Haugen <geirmash@gmail.com>
2018-04-28 02:33:58 +0200

    added retrieval of tweets - functionality

---
+++ getAllTweets.py
@@ -0,0 +1,24 @@
+def getalltweets(twitter, username):
+	tweets = []
+	print('Getting tweets from user \"' + username + '\"...')
+
+	#getting the last 200 tweets
+	new_tweets = twitter.get_user_timeline(screen_name = username, count = 200)
+	tweets.extend(new_tweets)
+
+	oldest = tweets[-1]['id'] - 1
+
+	while len(new_tweets) > 0:
+		new_tweets = twitter.get_user_timeline(screen_name = username, count = 200, max_id = oldest)
+		tweets.extend(new_tweets)
+		oldest = extend(new_tweets)
+		oldest = tweets[-1]['id'] - 1
+		savetojson(username, tweets, '_tweets')
+
+#This function saves parameter data (json) to file.
+#Filename used is in following format: username_type.json where type is a filename format.
+def savetojson(username, data, type):
+	print('\nSaving json data to filename \"' + username + type + '\"...')
+	with open(username + type + '', 'a') as tf:
+		json.dump(data, tf)
+	print(str(len(data)) + ' entries written')
