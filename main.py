62dc40f6bddb23e44171d4c062b660613f9f367e
Geir Haugen <geirmash@gmail.com>
2018-04-28 02:26:02 +0200

    added lines for initializing the Twitter REST API

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python
+
+from twython import *
+
+app_token = open("consumer-key.apikey")
+app_secret = open("consumer-secret.apikey")
+auth_token = open("access-token.apikey")
+auth_secret = open("access-token-secret.apikey")
+
+
+twitter = Twython(
+	app_key=app_token,
+	app_secret=app_secret,
+	oauth_otken=auth_token,
+	oauth_otken_secret=auth_secret)
+
